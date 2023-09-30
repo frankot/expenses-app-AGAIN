b5e907da29bc32c9f821e8a3337991722fd8f6b7
frank <frankiantki@gmail.com>
2023-09-30 21:12:54 +0200

    added tailwind styling to expense ExpensItem

--- src/components/ExpensItem.jsx
+++ src/components/ExpensItem.jsx
@@ -1,10 +1,14 @@
-export default function ExpensItem() {
+import ExpenseDate from "./ExpenseDate";
+
+export default function ExpensItem(props) {
   return (
-    <div className="flex justify-between items-center mx-24 bg-green-700 rounded-md p-2">
-      <div className="flex-none mr-10">22 March 2022</div>
-      <div className="flex-1">Car Insurance</div>
-      <div className=" bg-purple-800 rounded-lg m-2 px-4 py-2 border">
-        <div>340 PLN</div>
+    <div className="flex justify-between items-center mx-24 my-5 bg-green-700 rounded-md p-2">
+      <div className="flex-none mr-10">
+        <ExpenseDate date={props.date} />
+      </div>
+      <div className="flex-1 text-3xl font-bold">{props.title}</div>
+      <div className=" bg-purple-800 rounded-lg m-2 px-4 py-2 border min-w-[8rem] font-bold text-2xl justify-center flex">
+        <div>{props.amount} PLN</div>
       </div>
     </div>
   );
--- src/components/ExpenseDate.jsx
+++ src/components/ExpenseDate.jsx
@@ -1,7 +1,12 @@
-export default function ExpenseDate (props) {
-    const month = props.date.toLocaleString("en-US", { month: "long" });
-    const year = props.date.getFullYear();
-    const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  
-
+export default function ExpenseDate(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  return (
+    <div className="flex flex-col items-center bg-slate-800 rounded-lg py-3 px-6 m-1 border">
+      <div className="font-bold text-lg">{month}</div>
+      <div>{year}</div>
+      <div className="text-3xl font-extrabold">{day}</div>
+    </div>
+  );
 }
