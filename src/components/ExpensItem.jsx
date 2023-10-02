334d3242aa9b3fed9964ee94aa0a02b6d2b3514c
frank <frankiantki@gmail.com>
2023-10-02 22:35:30 +0200

    sucesfully added Card wrapper using tailwind!

--- src/components/ExpensItem.jsx
+++ src/components/ExpensItem.jsx
@@ -1,8 +1,9 @@
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
 export default function ExpensItem(props) {
   return (
-    <div className="flex justify-between items-center mx-24 my-5 bg-slate-400 rounded-md p-2">
+    <Card className="flex justify-between items-center mx-24 my-5 bg-slate-400  p-2">
       <div className="flex-none mr-10">
         <ExpenseDate date={props.date} />
       </div>
@@ -10,6 +11,6 @@ export default function ExpensItem(props) {
       <div className=" bg-purple-800 rounded-lg m-2 px-4 py-2 border min-w-[8rem] font-bold text-2xl justify-center flex">
         <div>{props.amount} PLN</div>
       </div>
-    </div>
+    </Card>
   );
 }
