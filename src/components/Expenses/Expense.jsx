59801f6b83e2e6c2245f6af1d0e1ca7debe0a4d0
frank <frankiantki@gmail.com>
2023-10-06 23:17:38 +0200

    useState into

--- src/components/Expenses/Expense.jsx
+++ src/components/Expenses/Expense.jsx
@@ -2,12 +2,11 @@ import ExpensItem from "./ExpensItem";
 
 export default function Expense(prop) {
   const expense = prop.expense;
-
   return (
     <div className="bg-slate-700 mx-32 my-10 py-6 rounded-xl">
       <ul>
         {expense.map((expenseItem) => (
-          <li>
+          <li key={expenseItem.id}>
             <ExpensItem
               title={expenseItem.title}
               amount={expenseItem.amount}
