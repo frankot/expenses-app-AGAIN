3e485c3c8f80dbff9b46a6b56c0a6614ea5cfea4
frank <frankiantki@gmail.com>
2023-10-09 18:35:24 +0200

    added basic state form handlers

--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -1,21 +1,38 @@
+import { useState } from "react";
 import "./newExpenseInput.css";
 
 export default function NewExpense() {
+    
+  const [title, setTitle] = useState("");
+  const titleChangeHandler = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const [amount, setAmount] = useState("");
+  const amountChangeHandler = (event) => {
+    setAmount(event.target.value);
+  };
+
+  const [date, setDate] = useState("");
+  const dateChangeHandler = (event) => {
+    setDate(event.target.value);
+  };
+
   return (
     <div className="max-w-2xl mx-auto bg-purple-400 mt-6 p-4 rounded-lg text-lg">
       <form className="flex flex-col items-center relative">
         <div className="grid grid-cols-2 ">
           <div className="newExpenseInput">
             <label>Title</label>
-            <input type="text" />
+            <input type="text" onChange={titleChangeHandler} />
           </div>
           <div className="newExpenseInput">
             <label>Amount</label>
-            <input type="number" min="0.01" step="0.01" />
+            <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
           </div>
           <div className="newExpenseInput">
             <label>Date</label>
-            <input type="date" min="2019-01-01" max="2023-12-31" />
+            <input type="date" min="2019-01-01" max="2023-12-31" onChange={dateChangeHandler}/>
           </div>
         </div>
         <div className="absolute bottom-0 right-0 rounded-lg bg-purple-700 hover:bg-purple-900 duration-300 py-2 px-4">
