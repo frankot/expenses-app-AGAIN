c23f5599eeea58244f1a2069deb4882fd1c6c70d
frank <frankiantki@gmail.com>
2023-10-15 20:26:04 +0200

    outptting conidtional content

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ export default function App() {
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expense expense={expenses} />
+      <Expense expenseData={expenses} />
     </div>
   );
 }
--- src/components/Expenses/Expense.jsx
+++ src/components/Expenses/Expense.jsx
@@ -1,30 +1,39 @@
 import { useState } from "react";
-import Card from "../UI/Card";
 import ExpensItem from "./ExpensItem";
 import ExpensesFilter from "./ExpensesFilter";
 
-export default function Expense(prop) {
-  const [filterdYear, setFilteredYear] = useState("2020");
+export default function Expense(props) {
+  const [filterdYear, setFilteredYear] = useState("2021");
   const savedYearHandler = (enteredYear) => {
     setFilteredYear(enteredYear);
-    console.log(`Filtered year from expenses: ${enteredYear}`);
   };
+  const filteredExpenses = props.expenseData.filter((item) => {
+    return item.date.getFullYear().toString() === filterdYear;
+  });
+  let expensesContent = (
+    <p className="text-center text-lg fontbold">
+      No expenses in year {filterdYear}
+    </p>
+  );
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expenseItem) => (
+      <ExpensItem
+        key={expenseItem.id}
+        title={expenseItem.title}
+        amount={expenseItem.amount}
+        date={expenseItem.date}
+      />
+    ));
+  }
 
   return (
-    <div className="">
+    <div>
       <ul>
         <ExpensesFilter
           selected={filterdYear}
           onSavePickedYear={savedYearHandler}
         />
-        {prop.expense.map((expenseItem) => (
-          <ExpensItem
-            key={expenseItem.id}
-            title={expenseItem.title}
-            amount={expenseItem.amount}
-            date={expenseItem.date}
-          />
-        ))}
+        {expensesContent}
       </ul>
     </div>
   );
